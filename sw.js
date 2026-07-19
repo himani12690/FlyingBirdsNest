6ae850f03fc64fc5ed735f75b166b4bf8d963f5f
himani12690 <himani12690@gmail.com>
2026-07-19 10:02:58 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='fbt-v9';
-const ASSETS=['./','./index.html','./manifest.json'];
+const CACHE='fbt-v10';
+const ASSETS=['./','./index.html','./manifest.json','./logo-round.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
